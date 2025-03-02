0b954eb03d10135236e4818396d2a57938028426
Bharani kumar reddy <63866826+bharanikumarreddy@users.noreply.github.com>
2025-03-02 12:52:51 -0500

    Update 88. Merge Sorted Array.py

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -16,10 +16,19 @@ class Solution:
                 nums1[i] = nums1[x]
                 x-=1
 
-
-
-
         """
         Do not return anything, modify nums1 in-place instead.
         """
         
+"""
+Imagine you have two lists of numbers, and you want to merge them into one sorted list. The tricky part is that you have extra space at the end of the first list (nums1), and you want to fill it from the back to avoid shifting elements forward (which is slow).
+
+To do this, we use three pointers:
+
+One at the end of valid numbers in nums1 (x = m - 1).
+One at the end of nums2 (y = n - 1).
+One at the very end of nums1 (last = m + n - 1).
+We start from the back and compare numbers from both lists. The larger number goes to the last empty position in nums1, and we move our pointer backward. If we run out of numbers in one list, we simply copy the remaining numbers from the other list.
+
+To make sure we go backward correctly, we use for i in range(last, -1, -1), which starts from the last index and moves step by step in reverse until we reach index 0. This way, we don’t mess up existing numbers in nums1 while merging
+"""
